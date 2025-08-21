98ebf2a958556e354bab7c8a5624847ba3db5403
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-08-21 18:07:39 -0300

    feat: add showValues(event) function

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ const parentheses = document.querySelector("#parentheses");
 const porcentage = document.querySelector("#porcentage");
 const division = document.querySelector("#division");
 const multiplication = document.querySelector("#multiplication");
-const subtraction = document.querySelector("#sbtraction");
+const subtraction = document.querySelector("#subtraction");
 const addition = document.querySelector("#addition");
 const signal = document.querySelector("#signal");
 const comma = document.querySelector("#comma");
 const equal = document.querySelector("#equal");
 
+keyboard.addEventListener("click", showValues);
 
+function showValues(event) {
+  let button = event.target;
+
+  if (button.id === "clear") {
+    result.value = "";
+  } else {
+    if (button.tagName === "BUTTON") {
+      result.value += button.innerText;
+    }
+  }
+}
