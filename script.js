86bff693ef88b652eb6048c2f3443910eed6af6f
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-08-21 17:38:13 -0300

    feat: start script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const result = document.querySelector("#result");
+const backspace = document.querySelector("#backspace");
+const keyboard = document.querySelector("#keyboard");
+const clear = document.querySelector("#clear");
+const parentheses = document.querySelector("#parentheses");
+const porcentage = document.querySelector("#porcentage");
+const division = document.querySelector("#division");
+const multiplication = document.querySelector("#multiplication");
+const subtraction = document.querySelector("#sbtraction");
+const addition = document.querySelector("#addition");
+const signal = document.querySelector("#signal");
+const comma = document.querySelector("#comma");
+const equal = document.querySelector("#equal");
+
+
